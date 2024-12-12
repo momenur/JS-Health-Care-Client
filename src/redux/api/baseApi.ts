b50e34a46d1f7f6e3024b566a1438feb61d63744
momenur <momenurislam6@gmail.com>
2024-12-12 23:09:47 +0600

    01

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,6 +3,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { tagTypesList } from "./tag-types";
 
 // Define a service using a base URL and expected endpoints
+
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
