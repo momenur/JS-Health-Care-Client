9cb916cf573f28e4682360956e4c1bdc2a3fa55c
momenur <momenurislam6@gmail.com>
2024-12-16 00:25:12 +0600

    added datepicker form MUX

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
@@ -1,8 +1,9 @@
+import JSDatePicker from "@/components/forms/JSDatePicker";
 import JSForm from "@/components/forms/JSForm";
 import JSModal from "@/components/shared/js_modal/JSModal";
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import React from "react";
-import { FieldValue } from "react-hook-form";
+import { FieldValues } from "react-hook-form";
 
 type TProps = {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,7 +11,8 @@ type TProps = {
 };
 
 const ScheduleModal = ({ open, setOpen }: TProps) => {
-  const handleFormSubmit = async (value: FieldValue) => {
+  const handleFormSubmit = async (values: FieldValues) => {
+    console.log(values);
     try {
     } catch (err: any) {
       console.error(err.message);
@@ -19,6 +21,11 @@ const ScheduleModal = ({ open, setOpen }: TProps) => {
   return (
     <JSModal open={open} setOpen={setOpen} title="Create Schedule">
       <JSForm onSubmit={handleFormSubmit}>
+        <Grid container spacing={2}>
+          <Grid item md={12}>
+            <JSDatePicker name="startDate" />
+          </Grid>
+        </Grid>
         <Button type="submit">Create</Button>
       </JSForm>
     </JSModal>
