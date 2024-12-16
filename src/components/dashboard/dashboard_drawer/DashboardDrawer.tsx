bce5a1545414a8cdf6d2af532f84c1036fc61367
momenur <momenurislam6@gmail.com>
2024-12-17 00:01:22 +0600

    added appbar

--- src/components/dashboard/dashboard_drawer/DashboardDrawer.tsx
+++ src/components/dashboard/dashboard_drawer/DashboardDrawer.tsx
@@ -9,6 +9,10 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { Avatar, Badge, Stack } from "@mui/material";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import { useGetSingleUserQuery } from "@/redux/api/userApi";
+import AccountMenu from "../account-menu/AccountMenu";
 import SideBar from "../side_bar/SideBar";
 
 const drawerWidth = 240;
@@ -36,6 +40,9 @@ export default function DashboardDrawer({
     }
   };
 
+  const { data, isLoading } = useGetSingleUserQuery({});
+  // console.log(data);
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -46,7 +53,8 @@ export default function DashboardDrawer({
           ml: { sm: `${drawerWidth}px` },
           background: "#F4F7FE",
           boxShadow: 0,
-          borderBottom: "1px solid lightgray",
+          borderBottom: "1px solid #ddd",
+          py: 1,
         }}
       >
         <Toolbar>
@@ -55,22 +63,45 @@ export default function DashboardDrawer({
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" }, color: "primary.main" }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
-            <MenuIcon />
+            <MenuIcon sx={{ color: "primary.main" }} />
           </IconButton>
-          <Box>
-            <Typography variant="body2" noWrap component="div" color="gray">
-              hi, Momenul Islam
-            </Typography>
-            <Typography
-              variant="body2"
-              noWrap
-              component="div"
-              color="primary.main"
-            >
-              Wellcome To, JS Doctor
-            </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <Box>
+              <Typography
+                variant="body2"
+                noWrap
+                component="div"
+                sx={{ color: "rgba(11, 17, 52, 0.6)" }}
+              >
+                Hi, {isLoading ? "Loading..." : data?.name ?? "User Name"},
+              </Typography>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ color: "primary.main" }}
+              >
+                Welcome to PH Health Care!
+              </Typography>
+            </Box>
+            <Stack direction="row" gap={3}>
+              <Badge badgeContent={1} color="primary">
+                <IconButton sx={{ background: "#ffffff" }}>
+                  <NotificationsNoneIcon color="action" />
+                </IconButton>
+              </Badge>
+              <Avatar alt={data?.name} src={data?.profilePhoto} />
+              <AccountMenu />
+            </Stack>
           </Box>
         </Toolbar>
       </AppBar>
