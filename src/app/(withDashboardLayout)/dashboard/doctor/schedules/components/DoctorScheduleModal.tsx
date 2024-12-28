0332583c88a0a41ad9d21d9cfa129287eb233a28
momenur <momenurislam6@gmail.com>
2024-12-28 18:36:04 +0600

    added modal for doctor schedule

--- src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import { Box } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+import { useState } from "react";
 import JSModal from "@/components/shared/js_modal/JSModal";
-import { Box, Typography } from "@mui/material";
+import { useGetAllScheduleQuery } from "@/redux/api/scheduleApi";
+import MultipleSelectFieldChip from "./MultipleSelectFieldChip";
 
 type TProps = {
   open: boolean;
@@ -7,10 +16,41 @@ type TProps = {
 };
 
 const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
+  const [selectedDate, setSelectedDate] = useState(
+    dayjs(new Date()).toISOString()
+  );
+
+  const [selectedSchedulesIds, setSelectedSchedulesIds] = useState();
+  const query: Record<string, any> = {};
+  if (!!selectedDate) {
+    query["startDate"] = dayjs(selectedDate)
+      .hour(0)
+      .minute(0)
+      .millisecond(0)
+      .toISOString();
+    query["endDate"] = dayjs(selectedDate)
+      .hour(23)
+      .minute(59)
+      .millisecond(999)
+      .toISOString();
+  }
+
+  const { data, isLoading } = useGetAllScheduleQuery(query);
+  const schedules = data?.schedule;
+
   return (
     <Box>
       <JSModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
-        <Typography> This is Doctor Modal</Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label="Controlled picker"
+            value={dayjs(selectedDate)}
+            onChange={(newValue) =>
+              setSelectedDate(dayjs(newValue).toISOString())
+            }
+          />
+        </LocalizationProvider>
+        <MultipleSelectFieldChip schedules={schedules} />
       </JSModal>
     </Box>
   );
