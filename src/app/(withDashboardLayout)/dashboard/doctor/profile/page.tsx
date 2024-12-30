8a3712b005e854cf9ea76442e04457d0fe33a1d3
momenur <momenurislam6@gmail.com>
2024-12-30 21:55:57 +0600

    added doctor profile page

---
+++ src/app/(withDashboardLayout)/dashboard/doctor/profile/page.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const DoctorProfilePage = () => {
+  return (
+    <Box>
+      <Typography>This is Doctor Profile Page</Typography>
+    </Box>
+  );
+};
+
+export default DoctorProfilePage;
