c16e93789b4447c2e0df301126f096fb98cb6802
momenur <momenurislam6@gmail.com>
2025-01-08 23:30:09 +0600

    fixed top rated doctor image

--- src/components/ui/HomePage/top_rated_doctors/TopRatedDoctors.tsx
+++ src/components/ui/HomePage/top_rated_doctors/TopRatedDoctors.tsx
@@ -46,6 +46,7 @@ const TopRatedDoctors = async () => {
                     height={300}
                     width={500}
                     alt="doctor photo"
+                    className="h-80"
                   />
                 </Box>
                 <CardContent>
