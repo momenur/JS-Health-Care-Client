b06eb8379c043eb7d646457bb338eb8bdbc86425
momenur <momenurislam6@gmail.com>
2025-03-26 23:22:57 +0600

    added functionality for reset password

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,123 +1,105 @@
 "use client";
 
-import KeyIcon from "@mui/icons-material/Key";
-import JSForm from "@/components/forms/JSForm";
-import JSInput from "@/components/forms/JSInput";
-import { userLogin } from "@/services/actions/userLogin";
-import { storeUserInfo } from "@/services/auth.services";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
-import { useState } from "react";
+import { authKey } from "@/constant/authKey";
+import { useResetPasswordMutation } from "@/redux/api/authApi";
+import { deleteCookies } from "@/services/actions/deleteCookies";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 import { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
+import KeyIcon from "@mui/icons-material/Key";
+import JSForm from "@/components/forms/JSForm";
+import JSInput from "@/components/forms/JSInput";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export const validationSchema = z.object({
-  password: z.string().min(6, "Password must be at least 6 character"),
+const validationSchema = z.object({
+  newPassword: z.string().min(6, "Must be at least 6 characters long"),
 });
-
 const ResetPasswordPage = () => {
-  const [error, setError] = useState("");
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+  const token = searchParams.get("token");
+  console.log({ id, token });
+  const router = useRouter();
 
-  const handleResetPassword = async (login_data: FieldValues) => {
-    toast("please wait...", { duration: 700 });
+  const [resetPassword] = useResetPasswordMutation();
+
+  useEffect(() => {
+    if (!token) return;
+    localStorage.setItem(authKey, token);
+  }, [token]);
+
+  const onSubmit = async (values: FieldValues) => {
+    const updatedData = { ...values, id };
 
     try {
-      const res = await userLogin(login_data);
-      if (res?.data?.accessToken) {
-        storeUserInfo({ accessToken: res?.data?.accessToken });
-        toast.success(res?.message);
+      const res = await resetPassword(updatedData);
+
+      if ("data" in res && res.data.status === 200) {
+        toast.success("Password Reset Successful");
+        localStorage.removeItem(authKey);
+        deleteCookies([authKey, "refreshToken"]);
+        router.push("/login");
       } else {
-        setError(res?.message);
+        throw new Error("Something Went Wrong, Try Again");
       }
-    } catch (err: any) {
-      console.log(err);
-      toast.error("something went wrong !");
+    } catch (error) {
+      toast.success("Something Went Wrong, Try Again");
     }
   };
 
   return (
-    <Container
+    <Box
       sx={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        px: 4,
+        py: 2,
+        maxWidth: 600,
+        width: "100%",
+        boxShadow: 1,
+        borderRadius: 1,
+        mx: "auto",
+        mt: { xs: 2, md: 10 },
       }}
     >
-      <Stack
-        sx={{
-          justifyContent: "center",
-          maxWidth: 600,
-          width: "100%",
-          alignItems: "center",
-        }}
-      >
+      <Stack alignItems="center" justifyContent="center">
         <Box
           sx={{
-            boxShadow: 1,
-            width: "100%",
-            borderRadius: 1,
-            p: 4,
-            textAlign: "center",
+            "& svg": {
+              width: 100,
+              height: 100,
+            },
           }}
         >
-          <Stack
-            sx={{
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Box
-              sx={{
-                "& svg": {
-                  width: 100,
-                  height: 100,
-                },
-              }}
-            >
-              <KeyIcon sx={{ color: "primary.main" }} />
-            </Box>
-            <Box>
-              <Typography variant="h6" fontWeight={600} py={2}>
-                Reset password
-              </Typography>
-            </Box>
-          </Stack>
-
-          <Box>
-            <JSForm
-              onSubmit={handleResetPassword}
-              resolver={zodResolver(validationSchema)}
-              defaultValues={{
-                password: "",
-              }}
-            >
-              <Grid container spacing={2}>
-                <Grid item md={12}>
-                  <JSInput
-                    name="newPassword"
-                    label="New password"
-                    type="password"
-                    fullWidth={true}
-                  />
-                </Grid>
-              </Grid>
-
-              <Button
-                sx={{
-                  margin: "20px 0",
-                }}
-                fullWidth={true}
-                type="submit"
-              >
-                Reset Password
-              </Button>
-            </JSForm>
-          </Box>
+          <KeyIcon sx={{ color: "primary.main" }} />
         </Box>
+        <Typography variant="h5" fontWeight={600} sx={{ mb: 2 }}>
+          Reset password
+        </Typography>
       </Stack>
-    </Container>
+      <JSForm
+        onSubmit={onSubmit}
+        defaultValues={{ newPassword: "" }}
+        resolver={zodResolver(validationSchema)}
+      >
+        <Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <JSInput
+              name="newPassword"
+              type="password"
+              label="New Password"
+              sx={{ mb: 2 }}
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+
+        <Button type="submit" sx={{ width: "100%", my: 2 }}>
+          Reset password
+        </Button>
+      </JSForm>
+    </Box>
   );
 };
 
