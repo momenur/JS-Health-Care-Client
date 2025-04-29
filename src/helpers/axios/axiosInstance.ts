f748caf73a675551ffa8fcca95bc3bec0f8e0915
momenur <momenurislam6@gmail.com>
2025-04-29 22:43:51 +0600

    remove unusefull comment

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -11,12 +11,8 @@ instance.defaults.headers.post["Content-Type"] = "application/json";
 instance.defaults.headers["Accept"] = "application/json";
 instance.defaults.timeout = 60000;
 
-//Copy form Axios Doc
-// Add a request interceptor
 instance.interceptors.request.use(
   function (config) {
-    // Do something before request is sent
-
     const accessToken = getFromLocalStorage(authKey);
     if (accessToken) {
       config.headers.Authorization = accessToken;
@@ -25,18 +21,13 @@ instance.interceptors.request.use(
     return config;
   },
   function (error) {
-    // Do something with request error
     return Promise.reject(error);
   }
 );
 
-// Add a response interceptor
 instance.interceptors.response.use(
   //@ts-ignore
   function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-
     const responseObject: TResponseSuccessType = {
       data: response?.data?.data,
       meta: response?.data?.meta,
@@ -44,9 +35,6 @@ instance.interceptors.response.use(
     return responseObject;
   },
   async function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-
     const config = error?.config;
     if (error?.response?.status === 500 && !config.sent) {
       config.sent = true;
@@ -62,7 +50,6 @@ instance.interceptors.response.use(
         message: error?.response?.data?.message || "Something Went Wrong !!!",
         errorMessages: error?.response?.data?.message,
       };
-      // return Promise.reject(error);
       return responseObject;
     }
   }
