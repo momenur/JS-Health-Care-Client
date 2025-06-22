d879e60f4633e363bdf2e5c64e5163a1f6ebf495
momenur <momenurislam6@gmail.com>
2025-06-22 23:26:54 +0600

    added fake data

--- src/components/ui/HomePage/top_rated_doctors/TopRatedDoctors.tsx
+++ src/components/ui/HomePage/top_rated_doctors/TopRatedDoctors.tsx
@@ -4,7 +4,6 @@ import {
   Card,
   CardActions,
   CardContent,
-  CardMedia,
   Container,
   Grid,
   Stack,
@@ -14,137 +13,131 @@ import Title from "../../common/Title";
 import Image from "next/image";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import Link from "next/link";
+import { doctors } from "@/components/about/helpers/uiData";
 
-const TopRatedDoctors = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/doctor?page=1&limit=3`
-  );
-  const { data: doctors } = await res.json();
+const TopRatedDoctors = async () => (
+  <Box
+    sx={{
+      py: {
+        xs: 10,
+        md: 14,
+        lg: 24,
+      },
+      backgroundColor: "rgba(20, 20, 20, 0.1)",
+      clipPath: {
+        xs: "none",
+        sm: "none",
+        md: "none",
+        lg: "polygon(0 0, 100% 25%, 100% 100%, 0 75%)",
+      },
+    }}
+  >
+    <Title
+      title="our top rated doctors"
+      sub_title="Access to expert physicians and surgeons, advanced technology"
+      sub_sub_title="and top-quality surgery facilities right here"
+    />
 
-  return (
-    <Box
-      sx={{
-        py: {
-          xs: 10,
-          md: 14,
-          lg: 24,
-        },
-        backgroundColor: "rgba(20, 20, 20, 0.1)",
-        clipPath: {
-          xs: "none",
-          sm: "none",
-          md: "none",
-          lg: "polygon(0 0, 100% 25%, 100% 100%, 0 75%)",
-        },
-      }}
-    >
-      <Title
-        title="our top rated doctors"
-        sub_title="Access to expert physicians and surgeons, advanced technology"
-        sub_sub_title="and top-quality surgery facilities right here"
-      />
-
-      <Container sx={{ mt: { xs: 5, md: 8, lg: 10 } }}>
-        <Grid container spacing={{ xs: 2, md: 3, lg: 4 }}>
-          {doctors?.map((doctor: any) => (
-            <Grid item xs={12} sm={6} lg={4} key={doctor?.id}>
-              <Card
+    <Container sx={{ mt: { xs: 5, md: 8, lg: 10 } }}>
+      <Grid container spacing={{ xs: 2, md: 3, lg: 4 }}>
+        {doctors?.slice(0, 3).map((doctor: any) => (
+          <Grid item xs={12} sm={6} lg={4} key={doctor?.id}>
+            <Card
+              sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box
                 sx={{
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
+                  height: "250px",
+                  overflow: "hidden",
                 }}
               >
-                <Box
-                  sx={{
-                    height: "250px", // fixed height
-                    overflow: "hidden", // hide overflow if image is larger
+                <Image
+                  src={doctor?.profilePhoto}
+                  height={300}
+                  width={500}
+                  alt="doctor photo"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    objectFit: "cover",
                   }}
+                />
+              </Box>
+
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="div">
+                  {doctor?.name}
+                </Typography>
+
+                <Typography
+                  variant="body2"
+                  sx={{ color: "text.secondary", mb: 1 }}
                 >
-                  <Image
-                    src={doctor?.profilePhoto}
-                    height={300}
-                    width={500}
-                    alt="doctor photo"
-                    style={{
-                      width: "100%",
-                      height: "auto",
-                      objectFit: "cover",
-                    }}
-                  />
-                </Box>
+                  {doctor?.qualification}, {doctor?.designation}
+                </Typography>
 
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {doctor?.name}
-                  </Typography>
-
+                <Stack
+                  mt={1}
+                  spacing={1}
+                  direction={{
+                    xs: "column",
+                    md: "column",
+                    lg: "row",
+                  }}
+                  justifyContent="space-between"
+                >
                   <Typography
-                    variant="body2"
-                    sx={{ color: "text.secondary", mb: 1 }}
-                  >
-                    {doctor?.qualification}, {doctor?.designation}
-                  </Typography>
-
-                  <Stack
-                    mt={1}
-                    spacing={1}
-                    direction={{
-                      xs: "column",
-                      md: "column",
-                      lg: "row",
+                    sx={{
+                      color: "text.secondary",
+                      display: "flex",
+                      alignItems: "center",
                     }}
-                    justifyContent="space-between"
                   >
-                    <Typography
-                      sx={{
-                        color: "text.secondary",
-                        display: "flex",
-                        alignItems: "center",
-                      }}
-                    >
-                      <LocationOnIcon sx={{ mr: 0.5 }} />
-                      {doctor?.address}
-                    </Typography>
-                    <Typography color="primary.main">
-                      Fee: {doctor?.apointmentFee}
-                    </Typography>
-                  </Stack>
-                </CardContent>
+                    <LocationOnIcon sx={{ mr: 0.5 }} />
+                    {doctor?.address}
+                  </Typography>
+                  <Typography color="primary.main">
+                    Fee: {doctor?.apointmentFee}
+                  </Typography>
+                </Stack>
+              </CardContent>
 
-                <CardActions
-                  sx={{
-                    px: 2,
-                    pb: 3,
-                    flexDirection: {
-                      xs: "column",
-                      md: "row",
-                    },
-                    gap: 1.5,
-                    alignItems: "stretch",
-                  }}
-                >
-                  <Button fullWidth size="small">
-                    Book Now
-                  </Button>
-                  <Button fullWidth variant="outlined" size="small">
-                    View Profile
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+              <CardActions
+                sx={{
+                  px: 2,
+                  pb: 3,
+                  flexDirection: {
+                    xs: "column",
+                    md: "row",
+                  },
+                  gap: 1.5,
+                  alignItems: "stretch",
+                }}
+              >
+                <Button fullWidth size="small">
+                  Book Now
+                </Button>
+                <Button fullWidth variant="outlined" size="small">
+                  View Profile
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
 
-        <Box textAlign="center" mt={{ xs: 4, md: 6 }}>
-          <Link href="/doctors">
-            <Button variant="outlined">View All</Button>
-          </Link>
-        </Box>
-      </Container>
-    </Box>
-  );
-};
+      <Box textAlign="center" mt={{ xs: 4, md: 6 }}>
+        <Link href="/doctors">
+          <Button variant="outlined">View All</Button>
+        </Link>
+      </Box>
+    </Container>
+  </Box>
+);
 
 export default TopRatedDoctors;
