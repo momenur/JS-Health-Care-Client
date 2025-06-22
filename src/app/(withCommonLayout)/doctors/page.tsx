9ea98b02b57b6db0d9f212388757b15a0b350818
momenur <momenurislam6@gmail.com>
2025-06-22 23:30:56 +0600

    doctors page

--- src/app/(withCommonLayout)/doctors/page.tsx
+++ src/app/(withCommonLayout)/doctors/page.tsx
@@ -1,54 +1,48 @@
-import DoctorCard from "@/components/ui/doctor/DoctorCard";
-import { Doctor } from "@/types/doctor";
-import DashedLine from "@/ui/doctor/DashedLine";
+// import DoctorCard from "@/components/ui/doctor/DoctorCard";
+// import { Doctor } from "@/types/doctor";
+// import DashedLine from "@/ui/doctor/DashedLine";
 import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 
-const Doctors = async () => {
-  const res = await fetch("http://localhost:5000/api/v1/doctor");
-
-  const { data } = await res.json();
-
-  return (
-    <Container
+const Doctors = async () => (
+  <Container
+    sx={{
+      my: 6,
+    }}
+  >
+    <Typography
+      variant="h1"
+      align="center"
       sx={{
-        my: 6,
-      }}
-    >
-      <Typography
-        variant="h1"
-        align="center"
-        sx={{
-          fontWeight: 600,
-          fontSize: "40px",
-          mb: 6,
-          position: "relative",
-          display: "inline-block",
+        fontWeight: 600,
+        fontSize: "40px",
+        mb: 6,
+        position: "relative",
+        display: "inline-block",
+        left: "50%",
+        transform: "translateX(-50%)",
+        "&:after": {
+          content: '""',
+          position: "absolute",
+          bottom: "-8px",
           left: "50%",
           transform: "translateX(-50%)",
-          "&:after": {
-            content: '""',
-            position: "absolute",
-            bottom: "-8px",
-            left: "50%",
-            transform: "translateX(-50%)",
-            width: "60px",
-            height: "4px",
-            backgroundColor: "#3b82f6",
-            borderRadius: "2px",
-          },
-        }}
-      >
-        All doctors of our clinic
-      </Typography>
-      {data?.map((doctor: Doctor, index: number) => (
+          width: "60px",
+          height: "4px",
+          backgroundColor: "#3b82f6",
+          borderRadius: "2px",
+        },
+      }}
+    >
+      All doctors of our clinic
+    </Typography>
+    {/* {data?.map((doctor: Doctor, index: number) => (
         <Box key={doctor?.id} sx={{ padding: 3, borderRadius: 2 }}>
           <DoctorCard doctor={doctor} />
           {index + 1 < data?.length && <DashedLine />}
         </Box>
-      ))}
-    </Container>
-  );
-};
+      ))} */}
+  </Container>
+);
 
 export default Doctors;
